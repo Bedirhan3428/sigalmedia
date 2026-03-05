1fd925cbde945ba59f841f9c1e9b591c9e13c3bf
Bedirhan3428 <abimer2350@gmail.com>
2026-03-06 02:21:37 +0300

    Vercel router fix eklendi

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 export const API_URL = "https://sigalmedia-backend-1.onrender.com";
-=======
-export const API_URL = "https://sigalmedia-backend-1.onrender.com";
->>>>>>> 4928e3fc9fa91755d373d9d06501a9858c6b11e5
+
