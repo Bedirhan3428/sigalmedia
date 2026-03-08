97dd86aa259754c58f99d3f7f5ae0e60d8563bce
Bedirhan <abimer2350@gmail.com>
2026-03-08 17:37:23 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'; // GoogleAuthProvider eklendi
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
@@ -10,5 +10,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth    = getAuth(app);
-export const storage = getStorage(app);
+
+export const auth           = getAuth(app);
+export const storage        = getStorage(app);
+export const googleProvider = new GoogleAuthProvider(); // Google sağlayıcısı oluşturuldu
