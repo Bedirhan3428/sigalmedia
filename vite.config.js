d30c1ae3c819ccba350909fe7156677e8fc5514a
Bedirhan3428 <abimer2350@gmail.com>
2026-03-11 23:16:24 +0300

    Claude'un bahsettiği kritik güvenlik ve paket güncellemeleri yapıldı

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-tf': ['@tensorflow/tfjs'],
+          'vendor-nsfw': ['nsfwjs'],
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    exclude: ['nsfwjs', '@tensorflow/tfjs']
+  }
 })
